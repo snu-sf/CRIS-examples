e16c56c77a05615fdade30b9a6b1250451d19f2f
Gil Hur <gil.hur@sf.snu.ac.kr>
2026-03-21 22:25:59 +0900

    remove deplicated

--- deprecated/cinc/Cinc_A.v
+++
@@ -1,5 +0,0 @@
-(* Require Import CRIS.
-Require Import ImpPrelude.
-Require Import SchHeader SchA SchTactics.
-Require Import MemHeader MemA.
-Require Import Cinc_H. *)
--- deprecated/incr/IncrHeader.v
+++
@@ -1,5 +0,0 @@
-(* Require Import Common.
-
-Module IncrHdr.
-  Definition incr := "incr".
-End IncrHdr. *)
