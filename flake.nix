e13d9e49f168f0ee7130a1359b09324aeb0407ad
damhiya <damhiya@gmail.com>
2026-06-15 18:50:16 +0900

    update nix flake config

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "CRIS development environment";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:snu-sf/nixpkgs/rocq-env-0.2";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -17,7 +17,7 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        coq = pkgs.coq_8_20;
+        coq = pkgs.coq_9_0;
         coqPackages = pkgs.mkCoqPackages coq;
         callPackage = pkgs.lib.callPackageWith (pkgs // params // set);
         params = {
@@ -27,16 +27,19 @@
         set = {
           inherit coq;
           paco = callPackage coqPackages.paco.override { version = "4.2.3"; };
-          ExtLib = callPackage coqPackages.ExtLib.override { version = "0.12.2"; };
+          ExtLib = callPackage coqPackages.ExtLib.override { version = "0.13.0"; };
           ITree = callPackage coqPackages.ITree.override { version = "5.2.1"; };
-          Ordinal = callPackage coqPackages.Ordinal.override { version = "0.5.4"; };
+          Ordinal = callPackage coqPackages.Ordinal.override { version = "0.5.6"; };
           stdpp = callPackage coqPackages.stdpp.override { version = "1.12.0"; };
           iris = callPackage coqPackages.iris.override { version = "4.4.0"; };
         };
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = pkgs.lib.attrValues set;
+          buildInputs = pkgs.lib.attrValues set ++ [
+            coqPackages.coq-lsp
+            pkgs.coqtail-mcp
+          ];
         };
       }
     );
